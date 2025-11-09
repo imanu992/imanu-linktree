cc2e928295a69cb31e9b11e6b62477f0d44cffca
iManu <sciarrettamanuel132@gmail.com>
2025-11-09 15:34:09 +0100

    Fixed some error, redirect, changed font

--- beaboxclub/script.js
+++ beaboxclub/script.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const yearSpan = document.getElementById("currentYear");
   const currentYear = new Date().getFullYear();
 
-    yearSpan.textContent = currentYear;
-    
-    //
+  yearSpan.textContent = currentYear;
+
+  // redirection
+  setTimeout(function () {
+    window.location.href = "https://discord.gg/mcnAf8eSZ8";
+  }, 1200);
 });
