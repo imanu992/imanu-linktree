0d3227b050a8d41e77b35789e9439b28c69df5ad
iManu <sciarrettamanuel132@gmail.com>
2025-11-09 15:22:57 +0100

    Added redirection to a server Discord

---
+++ beaboxclub/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("currentYear");
+  const currentYear = new Date().getFullYear();
+
+    yearSpan.textContent = currentYear;
+    
+    //
+});
